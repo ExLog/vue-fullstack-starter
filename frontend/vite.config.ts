99cee980b0e179836fddcfbc0780a05c8f1eb2cb
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-14 00:22:40 +0800

    add vite compression plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents from 'vite-plugin-components'
 import WindiCSS from 'vite-plugin-windicss'
+import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteComponents(), WindiCSS()]
+  plugins: [vue(), ViteComponents(), WindiCSS(), compress()]
 })
