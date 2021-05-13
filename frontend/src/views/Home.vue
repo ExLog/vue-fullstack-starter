df9793a5c74329ff8c9c717f9b1401807393b898
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-14 00:20:05 +0800

    add component auto import plugin

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,8 +9,6 @@
 </template>
 
 <script setup lang="ts">
-import Counter from "../components/Counter.vue"
-import Logo from "../components/Logo.vue"
 import { useRoute } from "vue-router"
 
 const route = useRoute()
