b9e194d3f4719b0ef60017426fe0869d5526d9dc
ExLog <55097092+ExLog@users.noreply.github.com>
2021-05-13 23:52:11 +0800

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>Vue Fullstack Starter</h1>
+	<Counter />
+</template>
+
+<script setup lang="ts">
+import Counter from "./components/Counter.vue"
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+	<h2>Count: {{ count }}</h2>
+	<button @click="count++">Add count</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const count = ref(0)
+</script>
